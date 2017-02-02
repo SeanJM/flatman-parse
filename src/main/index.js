9788fd2c790743c346424afa3a30cec1d01781f2
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-02 16:45:25 -0500

    added 'isComment', 'isDocType'

--- src/main/index.js
+++ src/main/index.js
@@ -1,21 +1,26 @@
-function parse(str) {
+function parse(string) {
   var props = {
     content : '',
-    string : str,
+    string : string,
     index : 0,
     anchor : 0,
     open : 0,
     closed : 0,
-    length : str.length,
+    length : string.length,
     nodes : []
   };
 
+
   for (; props.index < props.length; props.index++) {
     if (!isSpace(props.string[props.index])) {
-      if (isOpenTag(props.string, props.index)) {
+      if (isOpenTag(props)) {
         captureNode(props);
-      } else if (isText(props.string, props.index)) {
+      } else if (isText(props)) {
         captureText(props);
+      } else if (isComment(props)) {
+        captureComment(props);
+      } else if (isDocType(props)) {
+        captureDocType(props);
       }
     }
   }
