f60ee81fe7eda9c73286d9d0bb36a5751f189946
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-03 10:14:05 -0500

    Added the self closing tags from the HTML spec, SPACE

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,30 @@
-var SELF_CLOSING = {
-  img : true,
-  input : true,
+const SPACE = {
+  ' ' : true,
+  '\t' : true,
+  '\n' : true
+};
+
+const SELF_CLOSING = {
+  br : true,
   hr : true,
-  br : true
+
+  col : true,
+  img : true,
+  wbr : true,
+
+  area : true,
+  base : true,
+  link : true,
+  meta : true,
+
+  embed : true,
+  input : true,
+  param : true,
+  param : true,
+  track : true,
+
+  keygen : true,
+  source : true,
+
+  command : true,
 };
