493ab91678e444840c6607d24e9069bb1f0d1d39
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-02-09 11:58:55 -0500

    Added 'lore ipsum' to Acid Test

--- test/tests/acid_test.js
+++ test/tests/acid_test.js
@@ -3,6 +3,7 @@ const path = require('path');
 const parse = require('../../index.js');
 const jquery = fs.readFileSync(path.resolve('test/jquery-3.1.1.min.js'), 'utf8');
 const bootstrap = fs.readFileSync(path.resolve('test/bootstrap.min.css'), 'utf8');
+const loremIpsum = fs.readFileSync(path.resolve('test/lorem-ipsum.txt'), 'utf8');
 
 module.exports = {
   name : 'The Acid Test',
@@ -21,6 +22,22 @@ module.exports = {
     <style>${bootstrap}</style>
   </head>
   <body>
+    <div class="titlebar" id="titlebar">
+      <div class="titlebar_menu">
+        <div class="titlebar_menu_input">
+          <span class="icon icon-hamburger"></span>
+        </div>
+      </div>
+      <ul class="titlebar_menu-list">
+        <li class="titlebar_menu-list_item">Home</li>
+        <li class="titlebar_menu-list_item">Portfolio</li>
+        <li class="titlebar_menu-list_item">Social Media</li>
+        <li class="titlebar_menu-list_item">About</li>
+      </ul>
+      <div class="titlebar_logo"></div>
+      <div class="titlebar_control"></div>
+    </div>
+    <div class="content">${loremIpsum}</div>
     <script>${jquery}</script>
   </body>
 </HTML>
@@ -95,6 +112,79 @@ module.exports = {
         tagName : 'body',
         attributes : {},
         childNodes : [{
+          tagName : 'div',
+          attributes : {
+            className : 'titlebar',
+            id : 'titlebar'
+          },
+          childNodes : [{
+            tagName : 'div',
+            attributes : {
+              className : 'titlebar_menu'
+            },
+            childNodes : [{
+              tagName : 'div',
+              attributes : {
+                className : 'titlebar_menu_input'
+              },
+              childNodes : [{
+                tagName : 'span',
+                attributes : {
+                  className : 'icon icon-hamburger'
+                },
+                childNodes : []
+              }]
+            }]
+          }, {
+            tagName : 'ul',
+            attributes : {
+              className : 'titlebar_menu-list'
+            },
+            childNodes : [{
+              tagName : 'li',
+              attributes : {
+                className : 'titlebar_menu-list_item'
+              },
+              childNodes : [ 'Home' ]
+            }, {
+              tagName : 'li',
+              attributes : {
+                className : 'titlebar_menu-list_item'
+              },
+              childNodes : [ 'Portfolio' ]
+            }, {
+              tagName : 'li',
+              attributes : {
+                className : 'titlebar_menu-list_item'
+              },
+              childNodes : [ 'Social Media' ]
+            }, {
+              tagName : 'li',
+              attributes : {
+                className : 'titlebar_menu-list_item'
+              },
+              childNodes : [ 'About' ]
+            }]
+          }, {
+            tagName : 'div',
+            attributes : {
+              className : 'titlebar_logo'
+            },
+            childNodes : []
+          }, {
+            tagName : 'div',
+            attributes : {
+              className : 'titlebar_control'
+            },
+            childNodes : []
+          }]
+        }, {
+          tagName : 'div',
+          attributes : {
+            className : 'content'
+          },
+          childNodes : [loremIpsum]
+        }, {
           tagName : 'script',
           attributes : {},
           childNodes : [jquery]
