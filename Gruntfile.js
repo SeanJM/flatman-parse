347bb31961672935eb17b95a6c63a169bbac17b2
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-05-17 10:20:52 -0400

    Cleaned up grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,74 +5,39 @@ const config = JSON.parse(fs.readFileSync(path.resolve('grunt.json')));
 
 module.exports = function(grunt) {
   let tasks = [
-    'setup'
+    'readme'
   ];
 
-  require('./grunt/getFileTypes')(function (types) {
-    initConfig.pkg = grunt.file.readJSON('package.json');
-    grunt.initConfig(initConfig);
+  initConfig.pkg = grunt.file.readJSON('package.json');
+  grunt.initConfig(initConfig);
 
-    tasks.push(
-      'readme',
-      'flatman'
-    );
+  if (config.isProduction) {
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    tasks.push('uglify');
+  } else {
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    tasks.push('concat');
+  }
 
-    if (types.img.length) {
-      if (config.isProduction) {
-        grunt.loadNpmTasks('grunt-contrib-imagemin');
-        tasks.push('imagemin');
-      } else {
-        tasks.push('copy:images');
-      }
-    }
+  tasks.push('test');
 
-    if (types.svg.length) {
-      grunt.loadNpmTasks('grunt-svgstore');
-      tasks.push('svgstore');
-    }
-
-    if (types.sass.length) {
-      tasks.push('sass');
-      tasks.push('autoprefixer');
-      grunt.loadNpmTasks('grunt-autoprefixer');
-      grunt.loadNpmTasks('grunt-contrib-sass');
-      if (config.isProduction) {
-        grunt.loadNpmTasks('grunt-contrib-cssmin');
-      }
-    }
-
-    if (types.js && config.isProduction) {
-      grunt.loadNpmTasks('grunt-contrib-uglify');
-      tasks.push('uglify');
-    } else {
-      grunt.loadNpmTasks('grunt-contrib-concat');
-      tasks.push('concat');
-    }
-
-    tasks.push('test');
-
-    grunt.registerTask('setup', function () {
-      require('./grunt/setup')(this.async());
-    });
-
-    grunt.registerTask('flatman', function () {
-      require('./grunt/flatman').task(this.async());
-    });
-
-    grunt.registerTask('readme', function () {
-      require('./grunt/readme').task(this.async());
-    });
-
-    grunt.registerTask('test', function () {
-      require('./grunt/test').task(this.async());
-    });
-
-    if (!config.isProduction) {
-      grunt.loadNpmTasks('grunt-contrib-copy');
-      grunt.loadNpmTasks('grunt-contrib-watch');
-      tasks.push('watch');
-    }
-
-    grunt.registerTask('default', tasks);
+  grunt.registerTask('setup', function () {
+    require('./grunt/setup')(this.async());
   });
+
+  grunt.registerTask('readme', function () {
+    require('./grunt/readme').task(this.async());
+  });
+
+  grunt.registerTask('test', function () {
+    require('./grunt/test').task(this.async());
+  });
+
+  if (!config.isProduction) {
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    tasks.push('watch');
+  }
+
+  grunt.registerTask('default', tasks);
 };
--- grunt/initConfig.js
+++ grunt/initConfig.js
@@ -1,41 +1,18 @@
 const fs = require('fs');
-const flatman = require('./flatman');
 const readme = require('./readme');
 const test = require('./test');
 const scripts = require('./scripts');
-const css = require('./css');
-const images = require('./images');
 
 const config = JSON.parse(fs.readFileSync('grunt.json'));
 
 module.exports = {
-  copy : {
-    images : images.task.copy
-  },
-
-  sass : css.task.sass,
-
-  cssmin : css.task.cssmin,
-
   concat : scripts.task.concat,
 
   uglify : scripts.task.uglify,
 
-  autoprefixer : css.task.autoprefixer,
-
-  imagemin : images.task.imagemin,
-
-  svgstore : images.task.svgstore,
-
   watch : config.isProduction
     ? {}
     : Object.assign({
-    // Flatman
-    flatman : {
-      files : flatman.files,
-      tasks : ['flatman']
-    },
-
     readme : {
       files : readme.files,
       tasks : ['readme']
@@ -55,8 +32,6 @@ module.exports = {
       tasks: ['default']
     }
   },
-    scripts.task.watch,
-    css.task.watch,
-    images.task.watch
+    scripts.task.watch
   )
 };
