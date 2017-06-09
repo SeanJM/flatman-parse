cd1a48bfd7e2febf89a6fdef7e9521992757ef6f
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-06-09 11:12:56 -0400

    Added detection of greater than or space at the end of a self closing tag.

--- src/predicates/isSelfClosingTag.js
+++ src/predicates/isSelfClosingTag.js
@@ -5,17 +5,23 @@ function isSelfClosingTag(p) {
     .toLowerCase();
 
   return (
-        SELF_CLOSING[
-        str.substring(0, 2) ]
-    ||  SELF_CLOSING[
-        str.substring(0, 3) ]
-    ||  SELF_CLOSING[
-        str.substring(0, 4) ]
-    ||  SELF_CLOSING[
-        str.substring(0, 5) ]
-    ||  SELF_CLOSING[
-        str.substring(0, 6) ]
-    ||  SELF_CLOSING[
-        str ]
+    SELF_CLOSING[
+      str.substring(0, 2) ]
+      && (GT[str[2]] || SPACE[str[2]]) ||
+    SELF_CLOSING[
+      str.substring(0, 3) ]
+      && (GT[str[3]] || SPACE[str[3]]) ||
+    SELF_CLOSING[
+      str.substring(0, 4) ]
+      && (GT[str[4]] || SPACE[str[4]]) ||
+    SELF_CLOSING[
+      str.substring(0, 5) ]
+      && (GT[str[5]] || SPACE[str[5]]) ||
+    SELF_CLOSING[
+      str.substring(0, 6) ]
+      && (GT[str[6]] || SPACE[str[6]]) ||
+    SELF_CLOSING[
+      str ]
+      && (GT[str[2]] || SPACE[str[2]])
   );
 }
