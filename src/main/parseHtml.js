905f56c11f2e27e2a6e47ed468e9e8be94247e44
Sean MacIsaac <seanjmacisaac@gmail.com>
2017-12-22 16:19:20 -0500

    Merged with captureNode

---
+++ src/main/parseHtml.js
@@ -0,0 +1,141 @@
+const { SPACE, SELF_CLOSING } = require("../constants/");
+const isOpenTag               = require("../predicates/isOpenTag");
+const isText                  = require("../predicates/isText");
+const isComment               = require("../predicates/isComment");
+const isDocType               = require("../predicates/isDocType");
+const isXmlDeclaration        = require("../predicates/isXmlDeclaration");
+
+const captureComment          = require("./captureComment");
+const captureDocType          = require("./captureDocType");
+const clearString           = require("./clearString");
+const captureText             = require("./captureText");
+const captureXmlDeclaration   = require("./captureXmlDeclaration");
+const clearBlockComment       = require("./clearBlockComment");
+const clearComment            = require("./clearComment");
+const clearLineComment        = require("./clearLineComment");
+const clearRegExp             = require("./clearRegExp");
+const getNode                 = require("./getNode");
+const resetCapture            = require("./resetCapture");
+
+const isBlockComment          = require("../predicates/isBlockComment");
+const isClosedTag             = require("../predicates/isClosedTag");
+const isOpenComment           = require("../predicates/isOpenComment");
+const isLineComment           = require("../predicates/isLineComment");
+const isRegExp                = require("../predicates/isRegExp");
+const isSelfClosingTag        = require("../predicates/isSelfClosingTag");
+const isStringQuote           = require("../predicates/isStringQuote");
+
+function captureNode(p) {
+  var hasSlash = false;
+  var capture = true;
+  var innerTag = "";
+  var node;
+
+  // Get inner tag
+  p.open += 1;
+  p.i += 1;
+
+  while (p.str[p.i] !== ">" && p.str[p.i]) {
+    innerTag += p.str[p.i];
+    p.i += 1;
+  }
+
+  if (innerTag[innerTag.length - 1] === "/") {
+    innerTag = innerTag.substring(0, innerTag.length - 1);
+    hasSlash = true;
+  }
+
+  p.i += 1;
+  node = getNode(innerTag);
+  innerTag = "";
+
+  if (hasSlash && SELF_CLOSING[node.tagName] || SELF_CLOSING[node.tagName]) {
+    capture = false;
+    p.nodes.push(node);
+    resetCapture(p);
+    p.i -= 1;
+  } else if (hasSlash) {
+    throw new Error("Tag: '" + node.tagName + "' is not a self closing tag.");
+  }
+
+  while (p.i < p.length && capture) {
+    if (isStringQuote(p)) {
+      clearString(p);
+    }
+
+    if (isLineComment(p)) {
+      clearLineComment(p);
+    }
+
+    if (isBlockComment(p)) {
+      clearBlockComment(p);
+    }
+
+    if (isRegExp(p)) {
+      clearRegExp(p);
+    }
+
+    if (isOpenComment(p)) {
+      clearComment(p);
+    }
+
+    if (isSelfClosingTag(p)) {
+      p.open += 1;
+      p.closed += 1;
+    } else if (isOpenTag(p)) {
+      p.open += 1;
+    } if (isClosedTag(p)) {
+      p.closed += 1;
+    }
+
+    if (p.open - p.closed === 0) {
+      node.childNodes = parseHtml(p.content);
+      p.nodes.push(node);
+
+      // Go to the end of the closed tag
+      p.i = p.str.indexOf(">", p.i);
+      p.i -= 1;
+      resetCapture(p);
+      capture = false;
+    }
+
+    if (p.open > p.closed) {
+      p.content += p.str[p.i];
+    }
+
+    p.i += 1;
+  }
+}
+
+function parseHtml(str) {
+  var p = {
+    content : "",
+    str     : str,
+    i       : 0,
+    anchor  : 0,
+    open    : 0,
+    closed  : 0,
+    length  : str.length,
+    nodes   : []
+  };
+
+  for (; p.i < p.length; p.i++) {
+    if (!SPACE[p.str[p.i]]) {
+      if (isOpenTag(p)) {
+        captureNode(p);
+      } else if (isText(p)) {
+        captureText(p);
+      } else if (isComment(p)) {
+        captureComment(p);
+      } else if (isDocType(p)) {
+        captureDocType(p);
+      } else if (isXmlDeclaration(p)) {
+        captureXmlDeclaration(p);
+      }
+    }
+  }
+
+  return p.nodes;
+}
+
+module.exports = parseHtml;
